02fbbe9a04dc0bb539c5f2293362e244fad13746
guyet <guyet@DESKTOP-US6LVTD>
2023-07-07 21:59:33 -0700

    changing class names to animals

--- Easy-CSharp-Metaprogramming/ClassBuilder.cs
+++ Easy-CSharp-Metaprogramming/ClassBuilder.cs
@@ -1,26 +1,27 @@
 ﻿using Easy_CSharp_Metaprogramming;
 using System.Text;
+using static AccessModifier;
+using static ReturnType;
 
-public enum AccessModifier
+public static class AccessModifier
 {
-    Public,
-    Protected,
-    Internal,
-    Private,
-    ProtectedInternal,
-    PrivateProtected
+    public const string Public = "public";
+    public const string Protected = "protected";
+    public const string Internal = "internal";
+    public const string Private = "private";
+    public const string ProtectedInternal = "protected internal";
+    public const string PrivateProtected = "private protected";
 }
 
-public enum ReturnType
+public static class ReturnType
 {
-    Void,
-    Int,
-    String,
-    Bool,
-    Double,
-    Decimal,
-    DateTime
-    // You can add more types here as needed.
+    public const string Void = "void";
+    public const string Int = "int";
+    public const string String = "string";
+    public const string Bool = "bool";
+    public const string Double = "double";
+    public const string Decimal = "decimal";
+    public const string DateTime = "DateTime";
 }
 
 public class ClassBuilder
@@ -28,30 +29,26 @@ public class ClassBuilder
     private StringBuilder _classCode;
     private string _indent;
 
-    public ClassBuilder(string className, int indentSpaces, AccessModifier accessModifier = AccessModifier.Public)
+    public ClassBuilder(string className, int indentSpaces, string accessModifier = Public)
     {
         _classCode = new StringBuilder();
         _indent = GenerateIndent(indentSpaces);
 
-        _classCode.AppendLine($"{accessModifier.ToString().ToLower()} class {className}");
+        _classCode.AppendLine($"{accessModifier} class {className}");
         _classCode.AppendLine("{");
     }
 
-    public ClassBuilder AddProperty(string type, string name, AccessModifier accessModifier = AccessModifier.Public)
+    public ClassBuilder AddProperty(string type, string name, string accessModifier = Public)
     {
-        _classCode.AppendLine($"{_indent}{accessModifier.ToString().ToLower()} {type} {name} {{ get; set; }}");
+        _classCode.AppendLine($"{_indent}{accessModifier} {type} {name} {{ get; set; }}");
         _classCode.AppendLine();
         return this;
     }
 
-    public ClassBuilder AddMethod(ReturnType returnType, string methodName, AccessModifier accessModifier, string methodBody, ExceptionHandlerBuilder exceptionHandlerBuilder)
+    public ClassBuilder AddMethod(string returnType, string methodName, string accessModifier, string methodBody, 
+        ExceptionHandlerBuilder exceptionHandlerBuilder)
     {
-        return AddMethod(returnType.ToString().ToLower(), methodName, accessModifier, methodBody, exceptionHandlerBuilder);
-    }
-
-    public ClassBuilder AddMethod(string returnType, string methodName, AccessModifier accessModifier, string methodBody, ExceptionHandlerBuilder exceptionHandlerBuilder)
-    {
-        _classCode.AppendLine($"{_indent}{accessModifier.ToString().ToLower()} {returnType} {methodName}()");
+        _classCode.AppendLine($"{_indent}{accessModifier} {returnType} {methodName}()");
         _classCode.AppendLine($"{_indent}" + "{");
 
         if (exceptionHandlerBuilder != null)
